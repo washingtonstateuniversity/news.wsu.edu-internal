fe3c050490c055a992e5db0d5d0d5e28a0efd4e9
Phil Cable <pcable@wsu.edu>
2017-09-26 13:09:41 -0700

    Enqueue Source Sans Pro from Google Fonts

--- functions.php
+++ functions.php
@@ -1,3 +1,14 @@
 <?php
 
 include_once __DIR__ . '/includes/theme-images.php';
+
+add_action( 'wp_enqueue_scripts', 'internal_news_enqueue_scripts' );
+/**
+ * Enqueue custom styles.
+ *
+ * @since 0.0.1
+ */
+function internal_news_enqueue_scripts() {
+	wp_enqueue_style( 'source_sans_pro', '//fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,900,900i' );
+
+}
