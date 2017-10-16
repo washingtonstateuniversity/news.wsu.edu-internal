2e186b0a3168211f20bae08b2852a60b90a58f22
Jeremy Felt <jeremy.felt@gmail.com>
2017-10-16 11:43:12 -0700

    Remove initial attempt at home page curation
    Focus on changing card layouts instead.

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
 
 include_once __DIR__ . '/includes/theme-images.php';
-include_once __DIR__ . '/includes/curation.php';
 include_once __DIR__ . '/includes/content-syndicate.php';
 include_once __DIR__ . '/includes/featured-stories.php';
 include_once __DIR__ . '/includes/mailchimp.php';
