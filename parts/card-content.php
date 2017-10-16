fa190237b1c505b7bfaa659f3eeee6a84ad770f9
Jeremy Felt <jeremy.felt@gmail.com>
2017-10-16 16:01:44 -0700

    Do not display hidden category markup on category pages

--- parts/card-content.php
+++ parts/card-content.php
@@ -2,7 +2,7 @@
 global $is_top_feature, $is_river;
 ?>
 <article class="card card--news">
-	<?php if ( false === $is_top_feature ) { ?>
+	<?php if ( false === $is_top_feature && ! is_category() ) { ?>
 	<span class="card-categories"><?php
 	$category_html = '';
 	foreach ( get_the_category() as $category ) {
