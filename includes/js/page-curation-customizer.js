5c32df48a19bd1ae9390fc932f5947fe9fb51272
Jeremy Felt <jeremy.felt@gmail.com>
2017-10-24 14:03:47 -0700

    Update Customizer script code to meet code standards

--- includes/js/page-curation-customizer.js
+++ includes/js/page-curation-customizer.js
@@ -6,18 +6,18 @@ jQuery( document ).ready( function( $ ) {
 	page_curation.sortable();
 	page_curation.disableSelection();
 
-	page_curation.bind( "sortstop", function ( e, ui ) {
+	page_curation.bind( "sortstop", function( e, ui ) {
 		let sections = {};
 
-		$( e.target ).find( "li" ).each( function ( i, e ) {
+		$( e.target ).find( "li" ).each( function( i, e ) {
 			let section_id = $( this ).attr( "id" ).substr( 8 );
 
 			sections[ section_id ] = {};
-			sections[ section_id ]['count'] = $(this).find( ".section-count input" ).val();
-			sections[ section_id ]['classes'] = $(this).find( ".section-classes input" ).val();
+			sections[ section_id ].count = $( this ).find( ".section-count input" ).val();
+			sections[ section_id ].classes = $( this ).find( ".section-classes input" ).val();
 		} );
 
 		sections = JSON.stringify( sections );
-		$( 'input[data-customize-setting-link="page_curation"]' ).attr( "value", sections ).trigger( "change" );
+		$( "input[data-customize-setting-link='page_curation']" ).attr( "value", sections ).trigger( "change" );
 	} );
 } );
