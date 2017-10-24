e220ce47879a8aaf9c3de3eaabf5424ac13fe83c
Jeremy Felt <jeremy.felt@gmail.com>
2017-10-24 14:10:05 -0700

    Add the initial output of curated sections to front page

--- front-page.php
+++ front-page.php
@@ -4,7 +4,40 @@ get_header();
 ?>
 
 <main>
-	<?php var_dump( WSU\News\Internal\Page_Curation\get_sections() ); ?>
+	<?php
+
+	foreach ( WSU\News\Internal\Page_Curation\get_sections() as $section_slug => $front_section ) {
+		$is_top_feature = false;
+		$is_river = false;
+		$section_query = new WP_Query( array(
+			'posts_per_page' => (int) $front_section['count'],
+			'category_name' => $section_slug,
+		) );
+
+		if ( $section_query->have_posts() ) {
+			?>
+			<section class="row single gutter pad-top cat-sec">
+				<div class="column one">
+					<header>
+						<h2><?php echo esc_html( $front_section['name'] ); ?></h2>
+					</header>
+					<div class="deck">
+					<?php
+						while ( $section_query->have_posts() ) {
+							$section_query->the_post();
+
+							get_template_part( 'parts/card-content' );
+
+						}
+					?>
+					</div>
+				</div>
+			</section>
+			<?php
+		}
+	}
+
+	?>
 </main>
 
 <?php
