4c8007ef1b7f992543bf072b965dfa30711629a6
Jeremy Felt <jeremy.felt@gmail.com>
2017-10-27 11:03:17 -0700

    Display a post's featured status in the post list table

--- includes/featured-stories.php
+++ includes/featured-stories.php
@@ -5,6 +5,8 @@ namespace WSU\News\Internal\Featured_Stories;
 add_action( 'pre_get_posts', 'WSU\News\Internal\Featured_Stories\filter_query_for_featured_posts', 10 );
 add_action( 'add_meta_boxes', 'WSU\News\Internal\Featured_Stories\add_meta_boxes', 10 );
 add_action( 'save_post', 'WSU\News\Internal\Featured_Stories\save_post', 10, 2 );
+add_filter( 'manage_post_posts_columns', 'WSU\News\Internal\Featured_Stories\manage_posts_columns', 10, 1 );
+add_action( 'manage_post_posts_custom_column', 'WSU\News\Internal\Featured_Stories\manage_posts_custom_column', 10, 2 );
 
 /**
  * @param \WP_Query $query
@@ -82,5 +84,48 @@ function save_post( $post_id, $post ) {
 		update_post_meta( $post_id, '_news_internal_featured', 'yes' );
 	} else {
 		delete_post_meta( $post_id, '_news_internal_featured' );
+	}
+}
+
+/**
+ * Add a custom column to the posts list table for featured items.
+ *
+ * This also removes the comments column.
+ *
+ * @since 0.5.1
+ *
+ * @param array $post_columns
+ *
+ * @return array
+ */
+function manage_posts_columns( $post_columns ) {
+	unset( $post_columns['comments'] );
+	unset( $post_columns['date'] );
+	unset( $post_columns['wsu_last_updated'] );
+
+	$post_columns['item_featured'] = 'Featured Post';
+	$post_columns['date'] = 'Date';
+	$post_columns['wsu_last_updated'] = 'Last Updated';
+
+	return $post_columns;
+}
+
+/**
+ * Output a post's featured status in a custom list table column.
+ *
+ * @since 0.5.1
+ *
+ * @param string $column_name
+ * @param int    $post_id
+ */
+function manage_posts_custom_column( $column_name, $post_id ) {
+	if ( 'item_featured' === $column_name ) {
+		$featured = get_post_meta( $post_id, '_news_internal_featured', true );
+
+		if ( 'yes' !== $featured ) {
+			$featured = 'no';
+		}
+
+		echo esc_html( ucwords( $featured ) );
 	}
 }
