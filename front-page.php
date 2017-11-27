8f7eae66d667e134bd2bef33504203299235e3bf
Jeremy Felt <jeremy.felt@gmail.com>
2017-11-27 14:49:59 -0800

    Remove the word newsletter from email signup text

--- front-page.php
+++ front-page.php
@@ -33,7 +33,7 @@ get_header();
 				<div id="mc_embed_signup">
 					<form action="//wsu.us3.list-manage.com/subscribe/post?u=02b3cd67989caec2eec47f036&amp;id=21d653f16c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 						<div id="mc_embed_signup_scroll">
-							<label for="mce-EMAIL">Sign up for the WSU Insider daily email newsletter</label>
+							<label for="mce-EMAIL">Sign up for the WSU Insider daily email</label>
 							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
 							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_02b3cd67989caec2eec47f036_21d653f16c" tabindex="-1" value=""></div>
