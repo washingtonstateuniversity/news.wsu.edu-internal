1d0285001b962c4c22697c4163a12f56475ddbf2
Jeremy Felt <jeremy.felt@gmail.com>
2018-01-04 12:17:27 -0800

    Refactor selection and capture of featured posts slightly

--- includes/class-featured-posts-customizer-control.php
+++ includes/class-featured-posts-customizer-control.php
@@ -25,7 +25,7 @@ class Featured_Posts_Control extends \WP_Customize_Control {
 	 * @since 0.6.0
 	 */
 	public function render_content() {
-		$post_ids = $this->value();
+		$post_ids = explode( ',', $this->value() );
 
 		if ( ! empty( $this->label ) ) : ?>
 			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
@@ -73,47 +73,62 @@ class Featured_Posts_Control extends \WP_Customize_Control {
 			}
 		</style>
 
-		<div class="featured-posts-selection">
+		<div class="featured-post-selection">
+			<label for="featured-post-title">Find featured story</label>
+			<input id="featured-post-title" type="text" value="" />
+		</div>
+
+		<div class="selected-featured-posts">
 			<?php
 			for ( $i = 0; $i <= 4; $i++ ) {
 				if ( isset( $post_ids[ $i ] ) ) {
-					$value = get_the_title( $post_ids[ $i ] );
+					?>
+					<div class="featured-post-single" data-featured-post-id="<?php echo esc_attr( $post_ids[ $i ] ); ?>">
+						<p><?php echo esc_html( get_the_title( $post_ids[ $i ] ) ); ?></p>
+					</div>
+					<?php
 				} else {
-					$value = '';
+					?>
+					<div class="featured-post-empty">
+						No featured post selected for this area.
+					</div>
+					<?php
 				}
-				?>
-				<div class="featured-post-single">
-					<label for="featured-post-<?php echo esc_attr( $i ); ?>">Feature <?php echo esc_html( $i + 1 ); ?></label>
-					<input id="featured-post-<?php echo esc_attr( $i ); ?>" class="featured-post-input" type="text" value="<?php echo esc_html( $value ); ?>" />
-				</div>
-
-				<?php
 			}
-
 			?>
 		</div>
+
 		<input type="hidden" <?php $this->link(); ?> value="<?php echo wp_json_encode( $post_ids ); ?>"/>
+
 		<script>
 			jQuery( function() {
-				var featured_post_input = $( ".featured-post-input" );
+				var featured_post_input = $( "#featured-post-title" );
 				featured_post_input.autocomplete( {
 					source: "<?php echo esc_js( get_rest_url( get_current_blog_id(), '/insider/v1/featured' ) ); ?>",
 					minLength: 2,
 					// Add the selected item label to the input field rather than the value.
 					select: function( event, ui ) {
 						event.preventDefault();
-						$( this ).val( ui.item.label );
-						// add value to hidden input / order
+						var next_slot = $( ".featured-post-empty" ).first();
+
+						next_slot.removeClass( "featured-post-empty" ).addClass( "featured-post-single" );
+						next_slot.data( "featured-post-id", ui.item.value );
+						next_slot.html( "<p>" + ui.item.label + "</p>" );
+
+						$( this ).val( "" );
+
+						var post_ids = [];
+
+						$( ".featured-post-single" ).each( function() {
+							post_ids.push( $( this ).data( "featured-post-id" ) );
+						} );
+
+						$( "input[data-customize-setting-link='featured_posts']" ).attr( "value", post_ids ).trigger( "change" );
 					},
-					// Don't change the input until one is actually selected.
+					// Don't show selected titles in the input before selection.
 					focus: function( event, ui ) {
 						event.preventDefault();
 					}
-				} );
-
-				// Auto-highlight post title entry boxes whenever they're selected.
-				featured_post_input.on( "focus", function() {
-					$( this ).select();
 				} );
 			} );
 		</script>
