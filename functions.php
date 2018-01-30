c2f8cc01b36763a183fc1fdc5abbf9ec47eca017
Jeremy Felt <jeremy.felt@gmail.com>
2018-01-30 13:37:14 -0800

    Bump version 0.8.4

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ add_filter( 'spine_child_theme_version', 'internal_news_theme_version' );
  * @return string
  */
 function internal_news_theme_version() {
-	return '0.8.3';
+	return '0.8.4';
 }
 
 add_action( 'wp_enqueue_scripts', 'internal_news_enqueue_scripts' );
