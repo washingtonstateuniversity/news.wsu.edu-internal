9c50c540fa2da959232ec05dc1b6223ef8d19265
Jeremy Felt <jeremy.felt@gmail.com>
2018-03-29 11:57:49 -0700

    Don't show the default category on the top of single views

--- parts/single-layout.php
+++ parts/single-layout.php
@@ -5,13 +5,19 @@
 	<div class="column one">
 
 		<?php
+		$default_category = absint( get_option( 'default_category' ) );
 		$post_categories = wp_get_post_categories( get_the_ID() );
 
-		if ( $post_categories ) {
+		// Output a category list if categories are assigned and if the default category is not the only category.
+		if ( $post_categories && ! ( 1 === count( $post_categories ) && $default_category !== $post_categories[0] ) ) {
 			?>
 			<ul>
 			<?php
 			foreach ( $post_categories as $category ) {
+				// Don't show the default category (Uncategorized) on features.
+				if ( $default_category === $category ) {
+					continue;
+				}
 				?>
 				<li>
 					<a href="<?php echo esc_url( get_category_link( $category ) ); ?>"><?php echo esc_html( get_cat_name( $category ) ); ?></a>
