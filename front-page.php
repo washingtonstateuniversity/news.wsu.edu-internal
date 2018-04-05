0e05d50d117d7a4235e5bc934836cbeebe2fce2a
Jeremy Felt <jeremy.felt@gmail.com>
2018-04-05 14:39:22 -0700

    Implement new sigup bar mid-front page

--- front-page.php
+++ front-page.php
@@ -20,17 +20,6 @@ get_header();
 				<p class="insider-description">News and information for faculty and staff throughout the WSU system</p>
 			</div>
 			<div class="column two">
-				<div id="mc_embed_signup">
-					<form action="//wsu.us3.list-manage.com/subscribe/post?u=02b3cd67989caec2eec47f036&amp;id=21d653f16c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-						<div id="mc_embed_signup_scroll">
-							<label for="mce-EMAIL">Sign up for the WSU Insider daily email</label>
-							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_02b3cd67989caec2eec47f036_21d653f16c" tabindex="-1" value=""></div>
-							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-						</div>
-					</form>
-				</div>
 			</div>
 		</section>
 	<?php
@@ -106,6 +95,22 @@ get_header();
 					</header>
 					<?php echo do_shortcode( '[wsuwp_events output="wsu-events" date_format="F j" site="calendar.wsu.edu" count="3" tag="insider" cache_bust="1"]' ); ?>
 					<a href="https://calendar.wsu.edu" class="button">View all upcoming events</a>
+				</div>
+			</div>
+		</section>
+
+		<section class="row single gutter pad-top bg-none">
+			<div class="column one">
+				<div id="mc_embed_signup">
+					<form action="//wsu.us3.list-manage.com/subscribe/post?u=02b3cd67989caec2eec47f036&amp;id=21d653f16c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+						<div id="mc_embed_signup_scroll">
+							<label for="mce-EMAIL">Sign up for the WSU Insider daily email</label>
+							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="">
+							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_02b3cd67989caec2eec47f036_21d653f16c" tabindex="-1" value=""></div>
+							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+						</div>
+					</form>
 				</div>
 			</div>
 		</section>
