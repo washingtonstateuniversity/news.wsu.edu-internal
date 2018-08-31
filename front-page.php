375c14f91b021007fc666ae16ca6e65a38143e0c
jbickelhaupt <jbickelhaupt@wsu.edu>
2018-08-31 14:15:52 -0700

    v0.11.5-New MailChimp List
    Linking code to new MailChimp signup form. Form is then attached to a
    new MailChimp list.

--- front-page.php
+++ front-page.php
@@ -100,12 +100,12 @@ get_header();
 		<section class="row single gutter pad-top bg-none">
 			<div class="column one">
 				<div id="mc_embed_signup">
-					<form action="//wsu.us3.list-manage.com/subscribe/post?u=02b3cd67989caec2eec47f036&amp;id=21d653f16c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+					<form action="//wsu.us3.list-manage.com/subscribe/post?u=02b3cd67989caec2eec47f036&amp;id=c48ab7f5c3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
 						<div id="mc_embed_signup_scroll">
 							<label for="mce-EMAIL">Sign up for the WSU Insider daily email</label>
 							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="">
 							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_02b3cd67989caec2eec47f036_21d653f16c" tabindex="-1" value=""></div>
+							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_02b3cd67989caec2eec47f036_c48ab7f5c3" tabindex="-1" value=""></div>
 							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
 						</div>
 					</form>
